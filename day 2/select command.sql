59a532fb739ea8fd251b88c8320c8700d3261c40
Mujahid pasha <hmmujahid843@gmail.com>
2026-06-19 09:28:16 +0530

    day 3 commit

---
+++ day 2/select command.sql
@@ -0,0 +1,12 @@
+drop table employee;
+create table employee(
+  ID int,
+  name varchar(50),
+  salary decimal(2.2),
+  Mid int,
+  bonus int);
+
+
+insert into employee values('121','harsha','10000','1','2345'),
+('122','lalsab','11000','2','2365'),('123','prabhu','1000','3','234');
+select * from employee;
